b4ec1eb5b529f3f17924a0f1947e4dc81407c997
ShiYiXiaolong <gu.wolfauer@gmail.com>
2025-12-11 10:52:33 +0100

    update routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Test from './test.jsx'
 
-const pathname = window.location.pathname
-const Page = pathname === '/test' ? Test : App
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Page />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
-
