a4d08f5e2d026d4be133341cc331febaac719094
ShiYiXiaolong <gu.wolfauer@gmail.com>
2025-12-11 10:20:07 +0100

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Test from './test.jsx'
+
+const pathname = window.location.pathname
+const Page = pathname === '/test' ? Test : App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Page />
   </StrictMode>,
 )
 
